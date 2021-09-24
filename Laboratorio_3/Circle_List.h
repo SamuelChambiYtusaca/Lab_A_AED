c9b14612594430ecc52c1c45303aaedcf73fb683
SamuelChambiYtusaca <schambiyt@unsa.edu.pe>
2021-09-23 23:34:57 -0500

    Cambios finales Lista Circular

--- Laboratorio_3/Circle_List.h
+++ Laboratorio_3/Circle_List.h
@@ -11,13 +11,15 @@ class Circle_List{
         int size;
     public:
         Circle_List<T>();
-        Circle_List<T>(const Circle_List<T> &p);
-        Circle_List<T>(Circle_List<T> &&p);
         void push_back(T dato);
-        bool find(T dato);
-        void erase(T dato);
-        void printT();
-        void printD();
+        bool find(T dato);/*Indicador de existencia*/
+        void erase(T dato);/*Borrar dato por valor*/
+        void printT();/*Print ascendente*/
+        void printD();/*Print descendente*/
+        int get_size();
+        /*Sobrecarga de Operadores*/
+        const Circle_List<T> &operator= (const Circle_List<T> &);
+        Circle_List<T> operator+ (const Circle_List<T> &); /*Concatenacion de listas con sobrecarga de operador de adicion*/ 
 };
 
 template<typename T>
@@ -31,41 +33,157 @@ template <typename T>
 void Circle_List<T>::push_back(T dato){
     NodoD<T> *nuevo = new NodoD<T>(dato);
     if(!this->head){
-       this->head = nuevo;
-       this->last = nuevo;
+        this->head = nuevo;
+        this->last = nuevo;
+        this->head->set_sig(head);
+        this->head->set_ant(last);
     }else{
-       NodoD<T> *aux = this->head;
-       while(aux->get_sig()!=head){
-           aux = aux->get_sig();
-       }
-       this->last = nuevo;
-       aux->set_sig(last);
-    }
-    this->last->set_sig(head);
-    this->head->set_ant(last); 
-    size++;    
+        this->last->set_sig(nuevo);
+        nuevo->set_ant(last);
+        nuevo->set_sig(head);
+        this->last = nuevo;
+        this->head->set_ant(last);
+    }    
+    size++;
+    return; 
 }
 
+template<typename T>
+bool Circle_List<T>::find(T dato){
+    if(!this->head)
+        return false;
+    if(this->head->get_dato() == dato)
+        return true;
+    else{
+        NodoD<T> *aux = this->head->get_sig();
+        while(aux != this->head){
+            if(aux->get_dato() == dato)
+                return true;
+            aux = aux->get_sig();
+        }
+        return false;
+    }
+}
 
+template<typename T>
+void Circle_List<T>::erase(T dato){
+    if(!this->head)
+        return;
+    if(!find(dato))
+        return;
+    else{
+        NodoD<T> *aux = this->head;
+        if(aux->get_dato()==dato){
+            if(size==1){
+                delete head;
+                head = nullptr;
+                last = nullptr;
+                size--;
+                return;
+            }
+            aux = this->head->get_sig();
+            delete head;
+            head = aux;
+            head->set_ant(last);
+            last->set_sig(head);
+        }else if(this->last->get_dato() == dato){
+            aux = last->get_ant();
+            delete last;
+            last = aux;
+            head->set_ant(last);
+            last->set_sig(head);
+        }else{
+            aux = aux->get_sig();
+            while(aux != head){
+                if(aux->get_dato() == dato){
+                    NodoD<T> *aux2 = aux->get_sig();
+                    NodoD<T> *aux3 = aux->get_ant();
+                    delete aux;
+                    aux2->set_ant(aux3);
+                    aux3->set_sig(aux2);
+                    size--;
+                    return;
+                }
+                aux = aux->get_sig();
+            }
+        }
+        size--;
+        return;   
+    }
+}
 
+template<typename T>
+int Circle_List<T>::get_size(){
+    return this->size;
+}
 
+template<typename T>
+void Circle_List<T>::printT(){
+    if(!this->head)
+        return;
+    else{
+        NodoD<T> *aux = this->head;
+        while(aux != last){
+            cout << "[" << aux->get_dato() << "]->";
+            aux = aux->get_sig();
+        }
+        cout << "[" << aux->get_dato() << "]\n";
+        return;
+    }
+}
 
+template<typename T>
+void Circle_List<T>::printD(){
+    if(!this->head)
+        return;
+    else{
+        NodoD<T> *aux = this->last;
+        while(aux != head){
+            cout << "[" << aux->get_dato() << "]->";
+            aux = aux->get_ant();
+        }
+        cout << "[" << aux->get_dato() << "]\n";
+        return;
+    }
+}
 
+template<typename T>
+const Circle_List<T> &Circle_List<T>:: operator= (const Circle_List<T> &p){
+    NodoD<T> *aux{nullptr};
+    while(this->head){
+        this->last->set_sig(nullptr);
+        aux = this->head->get_sig();
+        delete head;
+        head = aux;
+    }
+    aux = p.head;
+    this->push_back(aux->get_dato());
+    aux = aux->get_sig();
+    while(aux != p.head){
+        this->push_back(aux->get_dato());
+        aux = aux->get_sig();
+    }
+    return *this;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+template<typename T>
+Circle_List<T> Circle_List<T>::operator+ (const Circle_List<T> &p){
+    Circle_List<T> nueva;
+    NodoD<T> *aux = this->head;
+    nueva.push_back(aux->get_dato());
+    aux = aux->get_sig();
+    while(aux!=head){
+        nueva.push_back(aux->get_dato());
+        aux = aux->get_sig();
+    }
+    aux = p.head;
+    nueva.push_back(aux->get_dato());
+    aux = aux->get_sig();
+    while(aux != p.head){
+        nueva.push_back(aux->get_dato());
+        aux = aux->get_sig();
+    }
+    return nueva;
+}
 
 #endif
--- Laboratorio_3/main.cc
+++ Laboratorio_3/main.cc
@@ -6,7 +6,23 @@ int main(){
     Circle_List<int> L1;
     L1.push_back(64);
     L1.push_back(3);
-    L1.push_back(361);
     L1.push_back(84);
+    cout << "Lista 1:\n";
+    L1.printT();
+    Circle_List<int> L2;
+    L2.push_back(789);
+    L2.push_back(97);
+    L2.push_back(314);
+    cout << "Lista 2:\n";
+    L2.printT();
+    Circle_List<int> L3;
+    L3 = L1 + L2;
+    cout << "Lista 3:\n";
+    cout << "Ascendente: "; L3.printT();
+    cout << "Descendente: ";L3.printD();
+    cout << "Comprobacion de funciones implementadas:\n";
+    cout << L3.find(84) << "\n";
+    L3.erase(3);
+    cout << L3.find(3) << "\n";
     return 1; 
 }
